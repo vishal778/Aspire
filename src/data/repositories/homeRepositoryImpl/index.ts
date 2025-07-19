33a2de73ff2b0ce065053d217f14f31e9bdea8cc
vishal-batra <vishalbatra2008@gmail.com>
2025-07-20 01:02:48 +0530

    New code starting

--- src/data/repositories/homeRepositoryImpl/index.ts
+++ src/data/repositories/homeRepositoryImpl/index.ts
@@ -1,6 +1,3 @@
 import {HomeRepository} from '../../../domain/repositories/home';
-import {homeDataSource} from '../../datasources/homeDataSource';
 
-export class HomeRepositoryImpl implements HomeRepository {
-  constructor(private apiDataSource: homeDataSource) {}
-}
+export class HomeRepositoryImpl implements HomeRepository {}
--- src/domain/usecases/homeUseCase/index.ts
+++ src/domain/usecases/homeUseCase/index.ts
@@ -1,10 +1,1 @@
-import {homeDataSource} from '../../../data/datasources/homeDataSource';
-import {HomeRepositoryImpl} from '../../../data/repositories/homeRepositoryImpl';
-import {HomeRepository} from '../../repositories/home';
-export class HomeUseCase {
-  constructor(private homeRepository: HomeRepository) {}
-}
-
-export const HomeUseCases = new HomeUseCase(
-  new HomeRepositoryImpl(new homeDataSource()),
-);
+export const homeUseCase = {};
