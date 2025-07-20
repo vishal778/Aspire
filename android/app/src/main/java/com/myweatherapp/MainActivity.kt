791e6fad27e91e3ca49daa04e80334b7c78e3140
vishal-batra <vishalbatra2008@gmail.com>
2025-07-21 00:02:01 +0530

    App name

--- android/app/src/main/java/com/myweatherapp/MainActivity.kt
+++ android/app/src/main/java/com/myweatherapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.aspireapp
+package com.Noon
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "AspireApp"
+  override fun getMainComponentName(): String = "Noon"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/myweatherapp/MainApplication.kt
+++ android/app/src/main/java/com/myweatherapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.aspireapp
+package com.Noon
 
 import android.app.Application
 import com.facebook.react.PackageList
